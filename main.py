a7e7bae7cb30500dce64912489c6a9a0357bf67a
TheNoobInventor <eduobijoro@mailfence.com>
2022-10-07 20:48:53 +0100

    Corrected variable name to be written to text file

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ while 1: # So that you can try balancing again when it falls
         gyro_sum = 0
         for _ in range(GYRO_CALIBRATION_LOOP_COUNT): # loop variable
             gyro_sensor_value = gyro_sensor.speed()
-            f.write(str(gyro_sensor))
+            f.write(gyro_sensor_value)
             gyro_sum += gyro_sensor_value
             if gyro_sensor_value > gyro_max_rate:
                 gyro_max_rate = gyro_sensor_value
